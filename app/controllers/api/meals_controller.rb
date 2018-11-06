383e097822c3a2b5a13873a394384a977b4e6cde
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-11-06 16:12:13 -0500

    show all meals in users works

--- app/controllers/api/meals_controller.rb
+++ app/controllers/api/meals_controller.rb
@@ -1,2 +1,36 @@
 class Api::MealsController < ApplicationController
+
+  def index
+    @meals = User.find(params[:user_id]).meals
+    render json: @meals
+  end
+
+  def create
+    @user = User.create!(user_params)
+    render json: @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update!(user_params)
+    render json: @user
+  end
+
+  def destroy
+    @user = User.find(params[:id]).delete
+    render status: :ok
+  end
+
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :cal_goal)
+  end
+
 end
