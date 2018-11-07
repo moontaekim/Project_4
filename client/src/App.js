aa6d2bed084387bb19d1f38fd5e328e718022462
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-11-06 20:25:25 -0500

    router in plcae to go to /users

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import landingPage from './components/landingPage';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <Router>
+        <Switch>
+          <Route to="/users" component={landingPage}/>
+        </Switch>
+      </Router>
     );
   }
 }
