72930c468f9d94aacd3631e7c7b5733c77f8bbc5
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-11-06 20:41:49 -0500

    axios call to get users works

--- client/src/components/landingPage.js
+++ client/src/components/landingPage.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+
 
 export default class landingPage extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount = async () => {
+    await this.fetchUsers()
+  }
+
+  fetchUsers = async () => {
+    const response = await axios.get('/api/users')
+    this.setState({users: response.data})
+  }
   render() {
     return (
       <div>
