b9d14ee52fdbb0a47230b17221de190b0b8a4d52
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-11-09 15:50:25 -0500

    fixed heroku error

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import UserPage from './components/UserPage';
+import LandingPage from './components/LandingPage';
 
 
 
@@ -9,7 +10,8 @@ class App extends Component {
     return (
       <Router>
         <Switch>
-          <Route path='/' component={UserPage}/>
+          <Route exact path = '/' component={LandingPage}/>
+          <Route path='/users/:user_id' component={UserPage}/>
         </Switch>
       </Router>
     );
