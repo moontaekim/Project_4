8f327869b875ba844e6119a019cf110823ba8f19
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-11-09 15:45:55 -0500

    uppercase dummy

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+
+
+export default class LandingPage extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount = async () => {
+    await this.fetchUsers()
+  }
+
+  fetchUsers = async () => {
+    const response = await axios.get('/api/users')
+    this.setState({users: response.data})
+  }
+  
+  render() {
+    const users = this.state.users.map((user) => {
+      return(
+        <Link to={`/users/${user.id}`}>User Pagess</Link>
+      )
+    })
+    
+    return (
+      <div>
+        {users}
+      </div>
+    )
+  }
+}
---
+++ client/src/components/UserPage.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import MealComponent from './MealComponent';
+
+
+export default class UserPage extends Component {
+  state = {
+    meals: [],
+    user: {},
+    dates: [],
+    selectedDate: "",
+  }
+
+  componentDidMount = async () => {
+    await this.getTodaysDate()
+    await this.fetchData()
+  }
+  
+  fetchData = async () => {
+    const userId = this.props.match.params.user_id
+    const response = await axios.get(`/api/users/${userId}/meals`)
+    const responseUser = await axios.get(`/api/users/${userId}`)
+    this.setState({ meals: response.data, user: responseUser.data })
+  }
+
+  getTodaysDate = async () => {
+    const month = await new Date().getMonth()
+    var day = await new Date().getDate()
+    if (day < 10) {
+      var day = `0` + `${day}`
+    }
+    const year = await new Date().getFullYear()
+    this.setState({ selectedDate: `${year}-${month + 1}-${day}` })
+  }
+
+  handleChange = (event) => {
+    this.setState({ selectedDate: event.target.value })
+  }
+
+  render() {
+
+    return (
+      <div>
+
+        <input type='date' name='date'
+        value={this.state.selectedDate}
+        onChange={this.handleChange}
+        />
+
+        <MealComponent 
+        userId = {this.props.match.params.user_id}
+        meals = {this.state.meals}
+        selectedDate = {this.state.selectedDate}
+        fetchData = {this.fetchData}
+        />
+
+      </div>
+    )
+  }
+}
